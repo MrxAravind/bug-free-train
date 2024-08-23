804dfe2f0f62f6170a972478eccb783053e285a9
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-08-23 17:06:08 +0000

    Bug Fixes

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
-wget -qq $CONF
 python3 combine.py
-<<<<<<< HEAD
-alist server & rclone serve webdav --log-file serve.log  --config rclone.conf --no-modtime combine: --addr :5244
-=======
-
+mega-webdav ./ --public
 nohup rclone serve webdav --config rclone.conf --no-modtime combine: --addr :8080 --vfs-cache-mode full --vfs-cache-max-age 1m0s --buffer-size 64M &
 alist admin set spidy
 alist server
->>>>>>> c82cea1 (Bug Fix)
