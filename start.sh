edfa02cfb1eb9577516f74e5f09c60252a1764c9
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-08-24 20:21:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 wget -qq $CONF
 python3 combine.py
+mega-login $EMAIL $PASS
 mega-webdav ./ --public
 nohup rclone serve webdav --config rclone.conf --no-modtime combine: --addr :8080 --vfs-cache-mode full --vfs-cache-max-age 1m0s --buffer-size 64M &
 alist admin set spidy
