f63b5355154e24179ab9b01edfdf60e717d512f4
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-08-29 08:33:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 wget -qq $CONF
-nohup mega-login $EMAIL $PASS && mega-webdav / --public &
+mega-login $EMAIL $PASS && mega-webdav / --public
 python3 combine.py
 nohup rclone serve webdav --config rclone.conf --no-modtime combine: --addr :8080 --vfs-cache-mode full --vfs-cache-max-age 1m0s --buffer-size 64M &
 nohup ngrok http --domain=comic-lovely-ram.ngrok-free.app 5244 &
