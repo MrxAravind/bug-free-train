fce4427629337baebaa139876fb5591ea34439ec
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-07 20:32:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python3 combine.py
 ngrok config add-authtoken 2XLq3mGSJbGO1A97c9ITxs4nNKo_7Mj6ubxS5Z71fJMXhqu9x
 nohup ngrok http --domain=amazingly-kind-sunbeam.ngrok-free.app 5244 &
 nohup alist admin set spidy & alist server &
-rclone serve webdav --config rclone.conf --no-modtime combine: --addr :8888 --vfs-cache-mode full --vfs-cache-max-age 1m0s --buffer-size 64M
+rclone serve webdav --config rclone.conf --no-modtime combine: --addr :8888 --vfs-cache-mode full --vfs-cache-max-age 1m0s --buffer-size 64M > rc.txt
